5bf44b10c045942fb7024a7ca152025854eb020b
Snisken <Mr.Snisken1@gmail.com>
2022-08-16 13:30:41 -0400

    Added location array and test popup

--- search.js
+++ search.js
@@ -1,1 +1,27 @@
 const searchField = document.getElementById("search-field");
+const locationArray = [
+    {location:"Boston", zipcode:"02131"},
+    {location:"Florida", zipcode:"85394"},
+    {location:"California", zipcode:"95312"},
+    {location:"Texas", zipcode:"52902"},
+    {location:"New York", zipcode:"29411"},
+]
+
+/*
+function searchClickHandler(){
+    console.log("hello");
+}
+*/
+searchField.addEventListener("click", function(){
+
+    searchPopup();
+    letterCheck();
+    arrayDataCheck();
+
+
+})
+
+
+function searchPopup(){};
+function letterCheck(){};
+function arrayDataCheck(){};
