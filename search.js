480d3d4400c20cfc11195b86feed2bfb4be3e695
Snisken <Mr.Snisken1@gmail.com>
2022-08-17 15:19:13 -0400

    Commented out daisyui input field in favor of custom input field, commented out excess js functions, added search functionality

--- search.js
+++ search.js
@@ -1,4 +1,6 @@
 const searchField = document.getElementById("search-field");
+const searchPopup = document.getElementById("search-popup");
+
 const locationArray = [
     {location:"Boston", zipcode:"02131"},
     {location:"Florida", zipcode:"85394"},
@@ -6,22 +8,47 @@ const locationArray = [
     {location:"Texas", zipcode:"52902"},
     {location:"New York", zipcode:"29411"},
 ]
+const location2 = [
+    "Boston",
+    "Florida",
+]
 
 /*
 function searchClickHandler(){
     console.log("hello");
 }
 */
-searchField.addEventListener("click", function(){
+//bind to input event instead of click event
+searchField.addEventListener("input", function(){
 
-    searchPopup();
+    //searchPopup();
     letterCheck();
-    arrayDataCheck();
+    //arrayDataCheck();
 
 
 })
 
 
-function searchPopup(){};
-function letterCheck(){};
-function arrayDataCheck(){};
+//function searchPopup(){};
+function letterCheck(){
+    let userInput = searchField.value;
+    //let userLetterArray = [];
+
+    for(let item of location2){
+        if(item.startsWith(userInput)){
+            searchPopup.classList.remove("hidden");
+            searchPopup.innerHTML = `${item}`;
+            console.log("MATCH")
+        }
+    }
+    if(userInput === ""){
+        searchPopup.classList.add("hidden");
+    }
+};
+
+
+/**
+ * This function would compare the locationArray with the array created by the array in the letterCheck function
+ * if string = string, show item
+ */
+//function arrayDataCheck(){};
